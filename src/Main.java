e76b67ef26fedd6aa48e6fe727c16de66d328f31
Klevtsov Andrey <andrey.tiger99@gmail.com>
2021-08-10 13:03:55 +0300

    Add homework for javaqa 2.1

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticket_price = 100;
+        int amount_for_one_bonus_mile = 20;
+        int bonus_total = ticket_price / amount_for_one_bonus_mile;
+        System.out.println(bonus_total);
+    }
+} 
